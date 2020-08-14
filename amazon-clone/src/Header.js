7a3953665460c3d1688a3cd568a34fd24c2d4d3c
hishambokhari <hish.b92@gmail.com>
2020-08-14 23:34:00 +0100

    Add: Search bar

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,16 +1,28 @@
 import React from 'react'
+import './Header.css';
+import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
   return (
     <nav className="header">
       {/* Logo on the left */}
-     <img 
-      className="header__logo" 
-      src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
-      alt=""
-     />
+      <Link to="/">
+        <img 
+        className="header__logo" 
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+        alt=""
+      />
+      </Link>
+     
       {/* Search box */}
+      <div className="header__search">
+        <input type="text" className="header__searchInput"/>
+        <SearchIcon className="header__searchIcon"/>
+      </div>
+      
       {/* 3 links */}
+      
       {/* Basket icon with number */}
     </nav>
   )
