db96817064ff56678246a60ce5ae886082c34ff7
hishambokhari <hish.b92@gmail.com>
2020-08-16 00:37:46 +0100

    Add: Homepage img

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
-      <h1>I AM THE HOME PAGE</h1>
+    <div className="home">
+      <img src="https://m.media-amazon.com/images/G/01/digital/video/sonata/Superhero_UK_Acquisition_FT_Apr_20/f4b165f5-678a-48bb-a049-11e7ddc2168d._UR3000,600_SX1500_FMjpg_.jpg" alt=""/>
     </div>
   )
 }
 
 export default Home
+
