c62029b37ff6284c4f5ba469285ca79d58b162af
hishambokhari <hish.b92@gmail.com>
2020-08-16 17:52:14 +0100

    Add: Product rows and columns

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -11,15 +11,58 @@ function Home() {
         alt=""
       />
       {/* Product id, title, price, rating, image */}
-      <Product 
-        id={12321341}
-        title="The lean Startup: How constant Innovation Creates Radically Successful Businesses Paperback"
-        price={11.96}
-        rating={5}
-        image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+      <div className="home__row">
+        <Product 
+          id={12321341}
+          title="The lean Startup: How constant Innovation Creates Radically Successful Businesses Paperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
         />
+        <Product 
+          id={12321341}
+          title="The lean Startup: How constant Innovation Creates Radically Successful Businesses Paperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+        />
+      </div>
+      
       {/* Product */}
+      <div className="home__row">
+        <Product 
+          id={12321341}
+          title="The lean Startup: How constant Innovation Creates Radically Successful Businesses Paperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+        />
+        <Product 
+          id={12321341}
+          title="The lean Startup: How constant Innovation Creates Radically Successful Businesses Paperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+        />
+        <Product 
+          id={12321341}
+          title="The lean Startup: How constant Innovation Creates Radically Successful Businesses Paperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+        />
+      </div>
 
+      <div className="home__row">
+        <Product 
+          id={12321341}
+          title="The lean Startup: How constant Innovation Creates Radically Successful Businesses Paperback"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+        />
+        
+      </div>
 
     </div>
   )
