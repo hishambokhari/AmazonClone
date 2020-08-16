08e8b4844cf1cc77b7ee23f4552477f72ba77616
hishambokhari <hish.b92@gmail.com>
2020-08-16 21:47:04 +0100

    WIP: React context API

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import './Product.css';
+import { useStateValue } from './StateProvider';
 
 function Product({ id, title, image, price, rating }) {
+
+  const [{basket}, dispatch] = useStateValue()
+
+  const addToBasket = () => {
+    // Add item to basket
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating
+      }
+    })
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -22,7 +40,7 @@ function Product({ id, title, image, price, rating }) {
       </div>
       
       <img src={image} alt=""></img>
-      <button>Add to basket</button>
+      <button onClick={addToBasket}>Add to basket</button>
     </div>
   )
 }
--- amazon-clone/src/reducer.js
+++ amazon-clone/src/reducer.js
@@ -1,14 +1,17 @@
 export const initialState = {
-  basket: []
+  basket: [],
 };
 
 function reducer(state, action) {
+  console.log(action);
   switch(action.type) {
     case 'ADD_To_BASKET':
       // Logic for adding item to basket
+      return { state};
       break;
     case 'REMOVE_FROM_BASKET':
       // Logic from removing from basket
+      return {state};
       break;  
     default:
       return state;  
