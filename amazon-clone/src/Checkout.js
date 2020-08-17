7f2b3bae270c3a3d1197d4d91ddc445655a6ca1f
hishambokhari <hish.b92@gmail.com>
2020-08-17 17:20:50 +0100

    Splitting checkout page into right and left sides

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -8,6 +8,7 @@ function Checkout() {
   
   return (
     <div className="checkout">
+      <div className="checkout__left">
       <img 
       className="checkout__ad" 
       src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" 
@@ -34,11 +35,16 @@ function Checkout() {
             rating={item.rating}
             />
           ))}
-          
+        </div>
+      )}
+      </div>
+      {basket.length > 0 && (
+        <div className="checkout__right">
+          <h1>Subtotal</h1>
         </div>
       )}
     </div>
-  )
+  );
 }
 
 export default Checkout
