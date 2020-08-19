ab146297f25e1e4778b224e8b69e905022ce8d31
hishambokhari <hish.b92@gmail.com>
2020-08-19 17:04:19 +0100

    login page styling

--- amazon-clone/src/Login.js
+++ amazon-clone/src/Login.js
@@ -18,15 +18,17 @@ function Login() {
        
        <form>
       <h5>E-mail</h5>
-      <input type="text"/>
+      <input type="email"/>
       <h5>Password</h5>
-      <input type="text"/>
-      <button>Sign In</button>
+      <input type="password"/>
+      <button className="login__signInButton">Sign In</button>
        </form>
 
        <p>
-       By signing-in you agree to Amazon's Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+       By signing-in you agree to Amazon's Conditions of Use & Sale.
+        Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
        </p>
+       <button className="login__registerButton">Create your Amazon Account</button>
      </div>
     </div>
   );
