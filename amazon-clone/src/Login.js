dd8b5992feacc8e1a1fa3c38cc13e9967a4751a5
hishambokhari <hish.b92@gmail.com>
2020-08-20 10:28:29 +0100

    Auth with firebase

--- amazon-clone/src/Login.js
+++ amazon-clone/src/Login.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Login.css"
 import { Link, useHistory } from "react-router-dom";
+import { auth } from 'firebase';
+
+
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = event => {
+    event.preventDefault(); // stops the refresh
+    //do the login logic here
+    auth.signInWithEmailAndPassword(email, password)
+    .then((auth)=> {
+      // logged in, redirect to home page
+
+    })
+    .catch(e => alert(e.message));
+  };
+
+  const register = event => {
+    event.preventDefault(); // stops the refresh
+    //do the register logic here
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(auth => {
+      //create a user and logged in...redirect to home page
+    })
+    .catch((e) => alert.apply(e.message));
+  };
+
   return (
     <div className="login">
      <Link to="/">
@@ -18,17 +45,17 @@ function Login() {
        
        <form>
       <h5>E-mail</h5>
-      <input type="email"/>
+      <input value={email} onChange={event => setEmail(event.target.value)} type="email"/>
       <h5>Password</h5>
-      <input type="password"/>
-      <button className="login__signInButton">Sign In</button>
+      <input value={password}onChange={event => setPassword(event.target.value)} type="password"/>
+      <button onClick={login} type="submit" className="login__signInButton">Sign In</button>
        </form>
 
        <p>
        By signing-in you agree to Amazon's Conditions of Use & Sale.
         Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
        </p>
-       <button className="login__registerButton">Create your Amazon Account</button>
+       <button  onClick={register} className="login__registerButton">Create your Amazon Account</button>
      </div>
     </div>
   );
